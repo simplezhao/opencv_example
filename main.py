91b720f9c2baf1cd6588d290d2e49643bbe5e99d
simple <zhaogang@smart-lifestyle.cn>
2018-03-02 16:46:56 +0800

    计算出点的坐标

--- main.py
+++ main.py
@@ -2,39 +2,24 @@ import cv2
 import numpy as np
 import time
 #cap = cv2.VideoCapture(1)
-img = cv2.imread('test_red2.png')
+img = cv2.imread('test_red3.png')
 kernal = np.ones((5,5), np.uint8)
 imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 # BGR
-while True:
-    #ret, frame = cap.read()
-    #cv2.line(frame, (0, 0), (640, 480), (255, 0, 0), 5)
-    #cv2.rectangle(frame, (384,0),(510,128),(0,255,0),-1)
-    #cv2.circle(frame, (447, 63), 63, (0, 0, 255), -1)
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    lower_red = np.array([0,43,46])
-    upper_red = np.array([10, 255, 255])
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-    erosion = cv2.erode(mask, kernal, iterations=1)
-    ret, thresh = cv2.threshold(imgray, 127, 255, 0)
-    contoures, hierarchy = cv2.findContours(thresh, 1, 2)
-    cnt = contoures[0]
-    M = cv2.moments(cnt)
+ret,thresh = cv2.threshold(imgray, 127, 255, 0)
+image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_LIST,
+                                              cv2.CHAIN_APPROX_SIMPLE)
 
-    cv2.imshow('img', img)
-    cv2.imshow('mask', mask)
-    cv2.imshow('erosion', erosion)
-    #cv2.imshow('video', frame)
-
-    key = cv2.waitKey(1)
-    if key & 0xff == ord('q'):
-        break
-    if key & 0xff == ord('s'):
-        print(frame.shape)
-        print(M)
-        print('save image')
-        #filename = str(int(time.time())) +'.png'
-        #cv2.imwrite(filename, frame)
+img = cv2.drawContours(img, contours, 1, (0,255,0), 3)
+cnt = contours[0]
+M = cv2.moments(cnt)
+print(M)
+cx = int(M['m10']/M['m00'])
+cy = int(M['m01']/M['m00'])
+print(cx,cy)
+cv2.circle(img,(cx,cy), 2, (255,0,0), -1)
+cv2.imshow('img', img)
+cv2.waitKey(0)
 
 #cap.release()
 cv2.destroyAllWindows()
